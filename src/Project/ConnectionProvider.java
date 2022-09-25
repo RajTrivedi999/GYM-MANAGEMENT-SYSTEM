410b8b55bccbdc28b09dc61e18705fd0bd2b9539
raj trivedi <113424414+RajTrivedi999@users.noreply.github.com>
2022-09-25 19:16:43 +0530

    Add files via upload

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author raj trivedi
+ */
+public class ConnectionProvider {
+    
+    public static Connection getCon(){
+       try
+       {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/gms","root","12qwaszx");
+           return con;
+       }
+       catch(Exception e){
+           return null;
+       }
+    }
+}
